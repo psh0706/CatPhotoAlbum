aac9faeb8dad0f34c69f8dcb3c5c9a65dc70f237
psh0706 <best8427@naver.com>
2022-04-15 23:38:42 +0900

    ImageView

---
+++ src/ImageView.js
@@ -0,0 +1,33 @@
+const IMAGE_PATH_PREFIX = "https://fe-dev-matching-2021-03-serverlessdeploymentbuck-t3kpj3way537.s3.ap-northeast-2.amazonaws.com/public"
+
+export default function ImageView({$app, initialState}) {
+    this.state = initialState;
+    this.$target = document.createElement('div');
+    this.$target.className = 'Modal ImageView';
+
+    $app.appendChild(this.$target);
+
+    this.setState = (nextState) => {
+        this.state = nextState;
+        this.render();
+    }
+
+    this.render = () => {
+        this.$target.innerHTML = `<div class="content">${this.state ? `<img src="${IMAGE_PATH_PREFIX}${this.state}">` : ""}</div>`;
+        this.$target.style.display = this.state ? 'block' : 'none';
+
+        this.$target.addEventListener("click", (e) => {
+            if (e.target.className === "Modal ImageView") { 
+                this.setState();
+            }
+        })
+
+        window.addEventListener("keyup", (e) => { 
+            if (e.key === "Escape") { 
+                this.setState();
+            } 
+        });
+    }
+
+    this.render();
+}
